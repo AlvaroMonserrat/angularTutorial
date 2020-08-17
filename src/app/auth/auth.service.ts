f034f5e8aaed83aa0473bbd6b23abc1c3f525cd2
alvar <alvaro.monserrat@gmail.com>
2020-08-16 21:36:24 -0400

    Deploy Web

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -4,6 +4,8 @@ import { catchError, tap } from 'rxjs/operators';
 import { throwError, BehaviorSubject } from 'rxjs';
 import { User } from './user.model';
 import { Router } from '@angular/router';
+import { environment } from '../../environments/environment';
+
 
 export interface AuthResponseData{
     idToken: string;
@@ -24,7 +26,7 @@ export class AuthService{
     constructor(private http: HttpClient, private router: Router){}
 
     signup(email: string, password: string){
-        return this.http.post<AuthResponseData>("https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyB1DLMdxBmdiG-wjVaBnooDRjmHhiTSitE",
+        return this.http.post<AuthResponseData>("https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=" + environment.firebaseAPIkey,
          {
             email: email,
             password: password,
@@ -49,7 +51,7 @@ export class AuthService{
     }
 
     login(email: string, password: string){
-        return this.http.post<AuthResponseData>("https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyB1DLMdxBmdiG-wjVaBnooDRjmHhiTSitE", {
+        return this.http.post<AuthResponseData>("https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=" + environment.firebaseAPIkey, {
             email: email,
             password: password,
             returnSecureToken: true
